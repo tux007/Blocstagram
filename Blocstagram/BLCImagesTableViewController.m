f2b26e64d94ed215331848e07c33082bdb5d1b98
Thomas Steimann <thomas.steimann@resolvex.ch>
2015-02-26 21:41:16 +0100

    tux007: Corrections from analysis

--- Blocstagram/BLCImagesTableViewController.m
+++ Blocstagram/BLCImagesTableViewController.m
@@ -430,6 +430,7 @@
 
 
 - (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
     NSIndexPath *indexPath = self.tableView.indexPathForSelectedRow;
     if (indexPath) {
         [self.tableView deselectRowAtIndexPath:indexPath animated:animated];
@@ -437,6 +438,7 @@
 }
 
 - (void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
     
 }
 
